d716d6ccd3b4791724bb40876c567b38706060ff
root <121950263@qq.com>
2019-04-02 01:06:37 +0800

    add add.php

---
+++ php/add.php
@@ -0,0 +1,33 @@
+<?php
+include_once 'crud.php';
+$crud = new CRUD();
+if(isset($_POST['save']))
+{
+ $fname = $_POST['fname'];
+ $lname = $_POST['lname'];
+ $city = $_POST['city'];
+ 
+ // insert
+    $crud->create($fname,$lname,$city);
+ // insert
+ 
+}
+
+if(isset($_GET['del_id']))
+{
+ $id = $_GET['del_id'];
+ $crud->delete($id);
+ header("Location: index.php");
+}
+
+if(isset($_POST['update']))
+{
+ $id = $_GET['edt_id'];
+ $fname = $_POST['fname'];
+ $lname = $_POST['lname'];
+ $city = $_POST['city'];
+ 
+ $crud->update($fname,$lname,$city,$id);
+ 
+}
+?>
