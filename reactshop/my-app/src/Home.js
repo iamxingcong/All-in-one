dbcb988bc8ad6d65118b8a4aa44e9ae736d7513a
root <121950263@qq.com>
2019-04-07 09:24:30 +0800

    add new project

---
+++ reactshop/my-app/src/Home.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+class Home extends React.Component{
+	constructor(props){
+		super(props);
+		this.state = {
+			error: null,
+			isLoaded: false,
+			items: []
+		}
+	}
+
+	componentDidMount(){
+		fetch("http://localhost/all-in-one/All-in-one/php/interface/wordpress_post.php")
+		.then(res => res.join())
+		.then(
+			(result) => {
+				this.setState({
+					isLoaded: true,
+					items: result
+				})
+			},
+			(error) => {
+				this.setState({
+					isLoaded: true,
+					error
+				})
+				
+			}
+		
+		)
+	
+	}
+
+	render(){
+		const {error, isLoaded, items} = this.state;
+		if( error ){
+	 		return  '<h2>erorr:{error.message}</h2>';	
+		} else if ( !isLoaded ){
+			return '<h1> loading.... </h1>';
+		
+		} else {
+		
+		 return (
+		 	<div>
+				 sfsf
+	
+	
+		 	</div>
+		 
+		 );
+		}
+	
+	}
+
+}
+
+export default Home;
