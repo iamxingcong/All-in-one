24b9528a5f8f8fb33a2111711a50c325ac4255f1
cong.xing <cong.xing@ybm100.com>
2019-04-08 14:11:23 +0800

    revise

--- rn/AwesomeProject/App.js
+++ rn/AwesomeProject/App.js
@@ -8,7 +8,7 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
-
+import News from './News';
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android:
@@ -21,7 +21,8 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
+       <News />
+        <Text style={styles.welcome}>Welsfdsfdsfd sfdsfdcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
