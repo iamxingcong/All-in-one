cd50739bcee013f2c4a0d4df676731e878e313a1
cong.xing <cong.xing@ybm100.com>
2019-04-08 17:48:33 +0800

    revise

--- php/interface/wordpress_conn.php
+++ php/interface/wordpress_conn.php
@@ -1,15 +1,13 @@
 <?php 
-<<<<<<< HEAD
+
 ini_set("display_errors", "On");
 
 error_reporting(E_ALL | E_STRICT);
 
 header("Access-Control-Allow-Origin: *");
 header("content-Type: application/json; charset=UTF-8");//字符编码设置  
-=======
-header('Access-Control-Allow-Origin: *');
-header("content-Type: application/json; charset=utf8");//字符编码设置  
->>>>>>> master
+
+
 
 
 define('DB_SERVER','localhost');
