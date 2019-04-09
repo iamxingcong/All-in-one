e91855c2eefb3f79ef696c31abd1681d2df55a20
cong.xing <cong.xing@ybm100.com>
2019-04-09 16:21:45 +0800

    revise

--- rn/AwesomeProject/App.js
+++ rn/AwesomeProject/App.js
@@ -1,8 +1,8 @@
- import React, { Component } from "react";
+import React, { Component } from "react";
 
 import { Image, FlatList, StyleSheet, Text, View } from "react-native";
 
-var REQUEST_URL = "https://raw.githubusercontent.com/facebook/react-native/0.51-stable/docs/MoviesExample.json";
+var REQUEST_URL = "https://shop.miido.com.cn/index/getZixunIndex.action?page=1&rows=10";
 
 export default class SampleAppMovies extends Component {
   constructor(props) {
@@ -26,7 +26,7 @@ export default class SampleAppMovies extends Component {
       .then(responseData => {
         // 注意，这里使用了this关键字，为了保证this在调用时仍然指向当前组件，我们需要对其进行“绑定”操作
         this.setState({
-          data: this.state.data.concat(responseData.movies),
+          data: this.state.data.concat(responseData.datainfo),
           loaded: true
         });
       });
@@ -42,7 +42,7 @@ export default class SampleAppMovies extends Component {
         data={this.state.data}
         renderItem={this.renderMovie}
         style={styles.list}
-        keyExtractor={item => item.id}
+        keyExtractor={item => item.addtime}
       />
     );
   }
@@ -61,12 +61,12 @@ export default class SampleAppMovies extends Component {
     return (
       <View style={styles.container}>
         <Image
-          source={{ uri: item.posters.thumbnail }}
+          source={{ uri: item.listimg1 }}
           style={styles.thumbnail}
         />
         <View style={styles.rightContainer}>
           <Text style={styles.title}>{item.title}</Text>
-          <Text style={styles.year}>{item.year}</Text>
+          <Text style={styles.year}>{item.content}</Text>
         </View>
       </View>
     );
@@ -87,19 +87,21 @@ var styles = StyleSheet.create({
     marginLeft: 10,
   },
   title: {
-    fontSize: 12,
-    marginBottom: 8,
-    textAlign: "left"
+ 
+    textAlign: "left",
+    lineHeight: 25
   },
   year: {
-    textAlign: "left"
+    textAlign: "left",
+ 
   },
   thumbnail: {
-    width: 53,
-    height: 81
+    width: 113,
+    height: 120,
+    marginLeft: 5
   },
   list: {
-    paddingTop: 20,
+    paddingTop: 10,
     backgroundColor: "#F5FCFF"
   }
 });
